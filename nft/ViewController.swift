286d6d36624119d136f4180363f77f7fedbb28b4
wajih942 <wajih.jemai@esprit.tn>
2021-11-02 11:04:38 +0100

    NFT first commit

---
+++ nft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nft
+//
+//  Created by Apple Esprit on 2/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ nftUITests/nftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  nftUITestsLaunchTests.swift
+//  nftUITests
+//
+//  Created by Apple Esprit on 2/11/2021.
+//
+
+import XCTest
+
+class nftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
