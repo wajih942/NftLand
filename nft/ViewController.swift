d615f04ef1025b22d66f1e9f68ce9359f7a23aa7
wajih942 <wajih.jemai@esprit.tn>
2021-11-07 07:23:34 -0600

    wajih

--- nft/ViewController.swift
+++ nft/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var creatorImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
