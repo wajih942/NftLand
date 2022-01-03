00cc6a91aca731667ac0385ecd75d36035d2cae3
wajih942 <wajih.jemai@esprit.tn>
2022-01-03 04:29:40 -0600

    commit

--- nft/View/ViewController.swift
+++ nft/View/ViewController.swift
@@ -2,87 +2,40 @@
 //  ViewController.swift
 //  nft
 //
-//  Created by Apple Esprit on 2/11/2021.
+//  Created by wajih on 1/3/22.
 //
 
 import UIKit
-
+import Lottie
 class ViewController: UIViewController {
 
-    //var
-    
-    
-    
-    
-    
-    
-    //iboutlets
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //ibactions
-    
-    
-    @IBAction func burgerButton(_ sender: Any) {
-        performSegue(withIdentifier: "hallTofunctionalitiesSegue", sender: self)
-    }
-    
-    
-    
-    @IBAction func searchButton(_ sender: Any) {
-    }
-    
-    
-    @IBAction func placeBidButton(_ sender: Any) {
-    }
-    
-    
-    @IBAction func viewItemButton(_ sender: Any) {
-    }
-    
-    
-    @IBAction func recentlyAddedButton(_ sender: Any) {
-    }
-    
-    @IBAction func allItemsButton(_ sender: Any) {
-    }
-    
-    @IBAction func advancedFilterButton(_ sender: Any) {
-    }
-    
-    @IBAction func moveBackwardButton(_ sender: Any) {
-    }
-    
-    @IBAction func moveForwardButton(_ sender: Any) {
-    }
-    
-    @IBAction func NFTLandButton(_ sender: Any) {
-    }
-    
-    @IBAction func connectWalletButton(_ sender: Any) {
-    }
-    
-    @IBAction func discoverButton(_ sender: Any) {
-    }
-    @IBAction func faqButton(_ sender: Any) {
-    }
-    
-    @IBAction func supportButton(_ sender: Any) {
-    }
-    @IBAction func demosButton(_ sender: Any) {
-    }
+    let animationView = AnimationView()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
 
+        // Do any additional setup after loading the view.
+    }
+    func setupAnimation()  {
+        animationView.animation = Animation.named("cards")
+        animationView.frame = view.bounds
+        animationView.backgroundColor = .white
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        view.addSubview(animationView)
+    }
+    @IBAction func button(_ sender: Any) {
+        setupAnimation()
+    }
+    /*
+     
+     // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
